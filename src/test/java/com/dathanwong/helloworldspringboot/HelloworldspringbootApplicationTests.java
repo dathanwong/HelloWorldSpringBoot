e8fa22e0b6890faaa7f8ad33c81d3d48c2968fae
dathwo@outlook.com <dathwo@outlook.com>
2020-06-05 16:36:11 -0700

    Initial commit

---
+++ src/test/java/com/dathanwong/helloworldspringboot/HelloworldspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dathanwong.helloworldspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloworldspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
